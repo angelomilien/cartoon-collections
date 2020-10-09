5cf14006db1039d879ca4b31aa7f776dc198081e
angelo milien <angelomilienjoseph@gmail.com>
2020-10-08 20:13:22 -0400

    almost Done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |item, index|
+    puts (index + 1).to_s + '. *' + item
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.each do |w|
+    true if w.length > 4
+    false if w.length <= 4
+  end
 end
 
 def find_the_cheese# code an argument here
