8db8242b89db4bf3f9fc036b2503e319bdeed4ac
angelo milien <angelomilienjoseph@gmail.com>
2020-10-10 16:02:24 -0400

    Done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,17 +5,22 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-
+  planeteer_calls.collect  do |element|
+    (element.capitalize!) + "!"
+  end
+   
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.each do |w|
-    true if w.length > 4
-    false if w.length <= 4
+  calls_long.any? do |str|
+    str.size > 4
   end
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(no_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+   
+  no_cheese.detect{|w| w == "cheddar"}
 end
