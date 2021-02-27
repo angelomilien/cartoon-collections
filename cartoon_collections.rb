86df533615df992efbd23d7877d94df1244ef9a0
angelo milien <angelomilienjoseph@gmail.com>
2021-02-27 11:49:01 -0500

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,19 +8,16 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect  do |element|
     (element.capitalize!) + "!"
   end
-   
 end
 
 def long_planeteer_calls(calls_long)
   calls_long.any? do |str|
     str.size > 4
   end
-
 end
 
 def find_the_cheese(no_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-   
   no_cheese.detect{|w| w == "cheddar"}
 end
